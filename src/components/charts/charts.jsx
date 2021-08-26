ae7e760240edc98bb8e9a28438ccdb294468c254
Jonathan Belden <jonathan.belden@bluehalo.com>
2021-08-26 14:43:22 -0600

    fully working, optimizations needed

--- src/components/charts/charts.jsx
+++ src/components/charts/charts.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from "react";
-import { fetchDailyData } from "../../api";
+import {fetchDailyData} from "../../api";
 import {Line, Bar} from "react-chartjs-2";
 import styles from "./charts.module.css";
 
-const Charts = () => {
+const Charts = ({data: {confirmed, deaths, recovered}, country}) => {
     const [dailyData, setDailyData] = useState([]);
     useEffect(() =>{
         const fetchAPI = async () => {
@@ -11,7 +11,7 @@ const Charts = () => {
         }
         console.log(dailyData);
         fetchAPI();
-    });
+    }, []);
 
     const lineChart = (
         dailyData.length
@@ -23,6 +23,7 @@ const Charts = () => {
                         data: dailyData.map(({confirmed}) => confirmed),
                         label: "Infected",
                         borderColor: "#3333ff",
+                        backgroundColor: "rgba(0, 0, 255, 0.1",
                         fill: true,
                     }, {
                         data: dailyData.map(({deaths}) => deaths),
@@ -35,9 +36,34 @@ const Charts = () => {
 
             />): null
     );
+
+    const barChart = (
+        confirmed
+        ? (
+            <Bar
+                data={{
+                    labels: ["Infected", "Recovered", "Deaths"],
+                    datasets: [{
+                        label: "People",
+                        backgroundColor: [
+                            "rgba(0, 0, 255, 0.5)",
+                            "rgba(0, 255, 0, 0.5)",
+                            "rgba(255, 0, 0, 0.5)",
+                        ],
+                        data: [confirmed.value, recovered.value, deaths.value]
+                    }]
+                }}
+                options={{
+                    legend: {display: false},
+                    title: {display: true, text: `Current state in ${country}`},
+                }}
+            />
+        ) : null
+    );
+
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
